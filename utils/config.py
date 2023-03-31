9059224f0568127869f7f7edd2fa03d7779f1cb4
meng1103 <100661718+meng1103@users.noreply.github.com>
2023-03-31 15:53:34 +0800

    config

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ def args_parser():
     parser = argparse.ArgumentParser()
     # federated argument
 
-
+    parser.add_argument('--FL', type=str, default='fedavg', help="FL name  fedavg/moon/fedprox/scaffold/feddyn")
     parser.add_argument('--test_bs', type=int, default=64, help="test batch_size")
     parser.add_argument('--lr', type=float, default=0.001, help="learning_rate")
     parser.add_argument('--lr_decay', type=float, default=0.995, help="learning_rate_decay")
@@ -27,7 +27,7 @@ def args_parser():
 
     parser.add_argument('--model', type=str, default='lenet', help="model name")
 
-    parser.add_argument('--dataset', type=str, default='cifar10_LDA', help="dataset name cifar10_LDA,cifar10_rate,"
+    parser.add_argument('--dataset', type=str, default='cifar_LDA', help="dataset name cifar_LDA,cifar_rate,"
                                                                            "cifar100_LDA,cifar100_rate")
     parser.add_argument('--num_classes', type=int, default=10, help="class num")
     parser.add_argument('--num_clients', type=int, default=100, help="client num")
