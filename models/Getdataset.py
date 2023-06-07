34e3f205056112d45637038a06106361936947d3
meng1103 <100661718+meng1103@users.noreply.github.com>
2023-06-07 11:04:50 +0800

    Update Getdataset.py

--- models/Getdataset.py
+++ models/Getdataset.py
@@ -28,11 +28,7 @@ class GetDataSet(object):
         self.train_data = None
         self.train_dict = {}
 
-        if self.args.dataset == 'mnist':
-            self.MnistDataSetConstruct(self.args)
-        elif self.args.dataset == 'cifar':
-            self.CifarDataSetConstruct(self.args)
-        elif self.args.dataset == 'cifar_rate' or self.args.dataset == 'mnist_rate':
+        if self.args.dataset == 'cifar_rate' or self.args.dataset == 'mnist_rate':
             self.RateDataSetConstruct(self.args)
         elif self.args.dataset == 'cifar_LDA' or self.args.dataset == 'cifar100_LDA':
             self.LDADataSetConstruct(self.args)
@@ -41,53 +37,6 @@ class GetDataSet(object):
 
         else:
             exit("Error: Getdataset ")
-
-
-    def CifarDataSetConstruct(self, args):
-        train_transform = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-            transforms.RandomHorizontalFlip()
-        ])
-        test_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-        self.train_data = datasets.CIFAR10('../data', train=True, download=False, transform=train_transform)
-        test_data = datasets.CIFAR10('../data', train=True, download=False, transform=test_transform)
-
-        self.test_loader = DataLoader(test_data, batch_size=args.test_bs, shuffle=True)
-
-        self.train_size = self.train_data.data.shape[0]
-        self.test_size = test_data.data.shape[0]
-
-        if args.iid:
-            train_split_dict = cifar_iid(self.train_data, args.num_clients)
-            self.train_dict = train_split_dict
-
-
-    def MnistDataSetConstruct(self, args):
-        train_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.1307], std=[0.3081])
-        ])
-        test_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.1307], std=[0.3081])
-        ])
-        self.train_data = datasets.MNIST('../data', train=True, download=False, transform=train_transform)
-        test_data = datasets.MNIST('../data', train=True, download=False, transform=test_transform)
-
-        self.test_loader = DataLoader(test_data, batch_size=args.test_bs, shuffle=True)
-
-        self.train_size = self.train_data.data.shape[0]
-        self.test_size = test_data.data.shape[0]
-
-        if args.iid:
-            train_split_dict = mnist_iid(self.train_data, args.num_clients)
-            self.train_dict = train_split_dict
-
 
 
     def LDADataSetConstruct(self, args):
@@ -144,6 +93,7 @@ class GetDataSet(object):
         elif args.dataset == 'cifar100_LDA' and args.non_alpha == 0.1:
             train_split_dict = cifar100_LDA01(args)
             self.train_dict = train_split_dict
+
 
         else:
             exit("Error: LDADataSetConstruct")
